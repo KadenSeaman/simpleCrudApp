c710d2788ff2698b6c0fd58afa8b49a6aa212ba7
KadenSeaman <kadencs24@outlook.com>
2024-09-21 00:21:46 -0600

    finished simple crud app backend

--- index.js
+++ index.js
@@ -2,22 +2,21 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
-app.use(express.json());
 
-const Product = require('./models/product.model.js');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}))
+
+const { ReturnDocument } = require('mongodb');
+const productRoute = require('./routes/product.route.js');
+
+
+//routes
+app.use('/api/products', productRoute);
 
 
 app.get('/', (req,res) => {
     res.send('Hello World');
-});
-
-app.post('/api/products', async (req, res) => {
-    try {
-        const product = await Product.create(req.body);
-        res.status(200).json(product);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
 });
 
 mongoose.connect(process.env.MONGO_URI)
